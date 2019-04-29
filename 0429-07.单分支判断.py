7313f31aeca50bce6437a12aa5a769a4cce5c4cb
blackplusy <happyming07@sina.com>
2019-04-29 16:19:45 +0800

    Add files via upload

---
+++ 0429-07.单分支判断.py
@@ -0,0 +1,5 @@
+#coding=utf-8
+name=input('请输入您的法号：')
+
+if name=='王健林':
+    print('思聪是我儿')
---
+++ 0429-09.多分支判断.py
@@ -0,0 +1,11 @@
+#coding=utf-8
+
+name=input('请输入您喜欢的主播：')
+if name=='大司马':
+    print('三角形打野')
+elif name=='张大仙':
+    print('不慌，吃口药！')
+elif name=='55开':
+    print('我没开挂！！')
+else:
+    print('混的还不如黑哥')
